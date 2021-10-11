903c192cf14213ea6ceb074527a06ad012c7faaa
stOne <silveredovoui@gmail.com>
2021-10-11 15:07:27 +0000

    Create DovStoneSymfonyBlogAdminBundlePairDBBased.php

---
+++ src/DovStoneSymfonyBlogAdminBundlePairDBBased.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: stOne
+ * Date: 11/10/2021
+ * Time: 15:07
+ */
+
+namespace DovStone\Bundle\SymfonyBlogAdminBundlePairDBBased;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DovStoneSymfonyBlogAdminBundlePairDBBased extends Bundle
+{
+}
